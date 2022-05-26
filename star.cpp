930f85b0940822e47f5adcf212b493f115febf55
Braxton Meyer <85606600+BMeyer-git@users.noreply.github.com>
2022-05-26 17:15:04 -0600

    Update star.cpp
    Added comments to the constructor so the rationale is clearer

--- star.cpp
+++ star.cpp
@@ -2,7 +2,7 @@
  * Header File:
  *    Star : Represents the stars in the background of the lunar lander game
  * Author:
- *    Braxton Meyer
+ *    Braxton Meyer and Carlos Lespin
  * Summary:
  *    Where the star is located and what phase of blinking it is in
  ************************************************************************/
@@ -14,11 +14,11 @@
   * STAR : CONSTRUCTOR WITH POINT
   * Initialize the star at the passed position
   *****************************************/
-
-
 Star::Star(Point newPosition)
 {
+	// Sets the phase to a random state so all the stars blink at different times
 	this->phase = rand() % 207 +1;
+	// Likewise, the position is randomized so the sky can be filled with an unpatterned array of stars
 	this->position = newPosition;
 }
 
